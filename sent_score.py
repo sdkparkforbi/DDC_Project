c63bb4810e2d54bf319d432bab117f268ad1d471
chg0630 <160381397+chg0630@users.noreply.github.com>
2024-10-26 18:19:28 +0900

    Create sent_score.py

--- sent_score.py
+++ sent_score.py
@@ -12,10 +12,12 @@ import os
 font_path = "C:/Windows/Fonts/NanumGothic.ttf"
 fontprop = fm.FontProperties(fname=font_path, size=10)
 
+api_key = os.getenv("db_password")
+
 # 데이터베이스 연결 정보
 db_host = '59.9.20.28'
 db_user = 'user1'
-db_password = 'user1!!'
+db_password = f'{api_key}'
 db_database = 'cuif'
 charset = 'utf8'
 
